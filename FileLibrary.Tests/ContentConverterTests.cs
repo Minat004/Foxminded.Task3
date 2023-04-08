b7d9c3652f2535c5f74028ca51c9fc6756e368ac
Minato <umi0n3@gmail.com>
2023-04-08 13:45:26 +0200

    tests

---
+++ FileLibrary.Tests/ContentConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace FileLibrary.Tests;
+
+public class ContentConverterTests
+{
+    [Theory]
+    [InlineData("15.23", 15.23, true)]
+    [InlineData("1", 1, true)]
+    [InlineData("A", 0, false)]
+    [InlineData("0", 0, true)]
+    [InlineData("0.000001.00001", 0, false)]
+    public void ToDecimalTest(string inputString, decimal expected, bool expectedTryParse)
+    {
+        var converter = new ContentConverter();
+        
+        var nfi = new NumberFormatInfo()
+        {
+            NumberDecimalSeparator = "."
+        };
+
+        var actualParse = converter.ToDecimal(inputString, nfi, out var actual);
+        
+        Assert.Equal(expectedTryParse, actualParse);
+        Assert.Equal(expected, actual);
+    }
+    
+}
